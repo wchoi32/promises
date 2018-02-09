d5f0c4da41e16110551b29e57cd4f5f13ada7876
WooSung Choi <johnwschoi@gmail.com>
2018-02-08 21:02:37 -0800

    callback and promise constructor

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,21 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, 'utf8', (err, data) => {
+  	if (err) { return callback(err) };
+	callback(err, data.split('\n')[0]);
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
+  request(url, (err, response) => {
+  	if (err) { return callback(err) };
+  	callback(err, response.statusCode);
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
